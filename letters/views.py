0577bfeb4eb3da177118ed01940fe831c309a317
jiwon <cutty82432@gmail.com>
2023-08-29 22:23:31 +0900

    [feat] Make My Letter Page

--- letters/views.py
+++ letters/views.py
@@ -10,7 +10,8 @@ def letter(request, letter_id):
     return
 
 def mypage(request):
-    all_letters = Letter.objects.all().order_by("-pub_date")
+    username = get_user(request)
+    all_letters = Letter.objects.filter(user=username).order_by("-pub_date")
     print(get_user(request))
     return render(request, 'letters/my_page.html', {'letter_list':all_letters, 'username':get_user(request)})
 
