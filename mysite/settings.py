2e6e95bddb07c2f3d8b12c00fe6a0fa8dac287c0
jiwon <cutty82432@gmail.com>
2024-01-02 16:21:14 +0900

    [fix] static root fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR)
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR /"static",
