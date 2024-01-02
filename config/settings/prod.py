0046ab883b830c18c5616c020804c74b72237ac0
jiwon <cutty82432@gmail.com>
2024-01-02 18:12:13 +0900

    [build]

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,6 +6,6 @@ STATICFILES_DIRS = [
     # 경로 추가, 필요에 따라 더 추가
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_URL = '/static/'
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
